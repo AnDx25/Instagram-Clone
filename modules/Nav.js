35042876a007854d0ebfa5aa3ff240b64db989c3
AnDx25 <a.dixit@iiitmanipur.ac.in>
2021-03-13 12:51:25 +0530

    Profile-Page

---
+++ modules/Nav.js
@@ -0,0 +1,14 @@
+const Nav=()=>`<nav>
+<a href="" class="logo">
+    <img  src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram-logo">
+</a>
+<input class="search" type="text" name="" id="" placeholder="Search">
+<span class="nav-links">
+    <a href="#"><i class="fas fa-home"></i></a>
+    <a href="#"><i class="fab fa-facebook-messenger"></i></a>
+    <a href="#"><i class="far fa-compass"></i></a>
+    <a href="#"><i class="far fa-heart"></i></a>
+</span>
+</nav>`;
+
+export default Nav
